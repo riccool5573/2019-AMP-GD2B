9baf8efcfdb151c0034c386f2384589a7dc3ed2e
riccool5573 <26188@ma-web.nl>
2019-09-20 13:59:09 +0200

    opdracht 2 klaar

--- html/02myArray/script.js
+++ html/02myArray/script.js
@@ -9,37 +9,26 @@ canvas.width = width;
 canvas.height = height;
 var Points = [];
 let counter = 0;
-let q = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let A = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let B = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let C = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let D = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let E = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let F = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let G = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let H = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let I = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-let J = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
-A.draw(context);
-B.draw(context);
-C.draw(context);
-D.draw(context);
-E.draw(context);
-F.draw(context);
-G.draw(context);
-H.draw(context);
-I.draw(context);
-J.draw(context);
-Points.push(A);
-Points.push(B);
-Points.push(C);
-Points.push(D);
-Points.push(E);
-Points.push(F);
-Points.push(G);
-Points.push(H);
-Points.push(I);
-Points.push(J);
+for(i = 0; i < 10; i++){
+    let i = new Point(new Vector2d(Math.random()*1000,Math.random()*400),100,Math.random()*30);
+    i.draw(context);
+    Points.push(i);
+}
+
+function reset(){
+
+  Points = [];
+  for(i = 0; i < 10; i++){
+      let i = new Point(new Vector2d(Math.random()*1000,Math.random()*400),"black",Math.random()*30);
+      i.draw(context);
+      Points.push(i);
+  }
+  for (let i = 0; i < Points.length; i++){
+    context.fillText(i + 1, Points[i].position.dx , Points[i].position.dy - 5 - Points[i].height);
+  }
+}
+
+
 console.log(Points);
 for (let i = 0; i < Points.length; i++){
   context.fillText(i + 1, Points[i].position.dx , Points[i].position.dy - 5 - Points[i].height);
@@ -54,6 +43,17 @@ window.addEventListener('click',(evt)=>{
       Points[counter].color = "red";
       Points[counter].draw(context);
       counter++;
-    }
+      if (counter == 10){
+        console.log("done");
 
+        while (counter > 0){
+        counter -= 1;
+        Points[counter].color = "black";
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        reset();
+
+      }
+      }
+    }
 })
